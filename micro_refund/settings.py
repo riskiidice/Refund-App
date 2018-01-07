ada7943a02af81702a65025cc546fea3c216274d
Tikumporn Wankvar <thesecondstageshow@gmail.com>
2018-01-07 15:04:34 +0700

    fix deploy

--- micro_refund/settings.py
+++ micro_refund/settings.py
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'micro_refund.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+     'default': {
+        dj_database_url.config(
+        default=config('DATABASE_URL'))
+    },
+    'dev': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
