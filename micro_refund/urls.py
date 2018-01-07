e8c901d9ab57590057e4e89346b01a94a4c6ef73
Tikumporn Wankvar <thesecondstageshow@gmail.com>
2018-01-07 15:18:52 +0700

    fix deploy

--- micro_refund/urls.py
+++ micro_refund/urls.py
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', include('refund.urls', namespace="refund") ),
     url(r'^refund/', include('refund.urls', namespace="refund") ),
     url(r'^admin/', admin.site.urls),
 ]
